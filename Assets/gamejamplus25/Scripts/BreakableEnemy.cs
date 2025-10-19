eea2a28cbb060801d4aef0be04b265391be215cd
Gabriel de Souza <9301442+gabrieldevsouza@users.noreply.github.com>
2025-10-18 21:46:33 -0300

    Level goal Debug

---
+++ Assets/gamejamplus25/Scripts/BreakableEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Collider))]
+public class BreakableEnemy : MonoBehaviour
+{
+    [Tooltip("Minimum collision relative speed required to break this enemy.")]
+    [SerializeField] private float impulseThreshold = 7.5f;
+
+    public event Action Broken;
+    public bool IsBroken { get; private set; }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (IsBroken) return;
+
+        if (c.relativeVelocity.magnitude >= impulseThreshold)
+        {
+            IsBroken = true;
+            Broken?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/gamejamplus25/Scripts/LevelGoal.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class LevelGoal : MonoBehaviour
+{
+    BreakableEnemy[] _targets;
+    int _remaining;
+
+    void Start()
+    {
+        _targets = FindObjectsOfType<BreakableEnemy>(true);
+        _remaining = _targets.Length;
+
+        foreach (var t in _targets)
+            if (t != null) t.Broken += OnEnemyBroken;
+
+        Debug.Log($"[LevelGoal] Started with {_remaining} targets.");
+    }
+
+    void OnEnemyBroken()
+    {
+        _remaining = Mathf.Max(0, _remaining - 1);
+        Debug.Log($"[LevelGoal] Target broken. Remaining: {_remaining}");
+
+        if (_remaining == 0)
+            Debug.Log("[LevelGoal] All enemies broken! Level complete!");
+    }
+}
